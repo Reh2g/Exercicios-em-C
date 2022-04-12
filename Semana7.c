b5a165c7a4cb349f224371cd20ee44d83ac8db01
Gabriel Missima <92278167+Reh2g@users.noreply.github.com>
2022-04-12 18:10:33 -0300

    Create Semana7.c

---
+++ Semana7.c
@@ -0,0 +1,23 @@
+/*
+
+Escreva as instruções necessárias para declarar uma variável que contenha os dados de uma pessoa, por exemplo, Maria: 
+
+altura = 1,63 m; peso = 59,5 kg e idade = 21 anos
+
+nessa ordem.
+
+*/
+
+struct  pessoa 
+
+{ 
+
+float  altura; 
+
+float  peso; 
+
+int  idade; 
+
+}; 
+
+struct   pessoa  Maria;
